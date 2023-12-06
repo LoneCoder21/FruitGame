3327a8c9f5e2e97e59a241afbc03c622d01a33d9
Abhinav Neelam <abhinavneelam949@gmail.com>
2023-12-06 14:17:17 -0800

    prettier config + responsive main page

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,14 @@ import Link from "next/link";
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-amber-100">
-            <section className="flex flex-col items-center justify-between p-24">
-                <h1 className="text-mxl text-rose-500 text-stroke-2 text-stroke-green-400 font-extrabold">
+        <main className="flex min-h-screen flex-col bg-amber-100">
+            <section className="flex flex-col items-center my-40">
+                <h1 className="text-xl text-center text-rose-500 text-stroke-2 text-stroke-green-400 font-extrabold sm:text-mxl">
                     Watermelon Game
                 </h1>
                 <Link
                     href="/play"
-                    className="text-base bg-amber-400 px-12 py-3 m-3 font-bold text-white rounded-full shadow-lg shadow-amber-600 drop-shadow-lg hover:bg-amber-500 hover:scale-110 text-stroke-1 text-stroke-amber-800"
+                    className="text-base bg-amber-400 px-12 py-3 m-3 font-bold text-white rounded-full shadow-lg shadow-amber-600 hover:bg-amber-500 hover:scale-110 text-stroke-1 text-stroke-amber-800"
                 >
                     Start Game
                 </Link>
