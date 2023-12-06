c777ace457e59d097392db4cc4638bfd73aae50c
Abhinav Neelam <abhinavneelam949@gmail.com>
2023-12-06 12:49:01 -0800

    change default font to cursive

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,13 @@ export default function Home() {
                 <h2 className="text-xl text-amber-600">Watermelon Game</h2>
                 <Link
                     href="/play"
-                    className="text-base bg-amber-400 px-12 py-3 text-white m-3 rounded-full shadow-lg shadow-amber-600 drop-shadow-lg hover:bg-amber-500"
+                    className="text-base bg-amber-400 px-12 py-3 text-zinc-700 m-3 rounded-full shadow-lg shadow-amber-600 drop-shadow-lg hover:bg-amber-500 hover:scale-110"
                 >
                     Start Game
                 </Link>
                 <Link
                     href="/help"
-                    className="text-base bg-emerald-300 px-12 py-3 text-white m-3 rounded-full shadow-lg shadow-emerald-500 hover:bg-emerald-400"
+                    className="text-base bg-emerald-300 px-12 py-3 text-zinc-700 m-3 rounded-full shadow-lg shadow-emerald-500 hover:bg-emerald-400 hover:scale-110"
                 >
                     How to Play
                 </Link>
